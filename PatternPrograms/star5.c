cc42fc90c1896037f7971865fc2777a544d27e44
PrathmShinde <99710720+PrathmShinde@users.noreply.github.com>
2023-04-21 17:45:18 +0530

    Add files via upload

---
+++ PatternPrograms/star5.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main(){
+
+
+	for(int i=1;i<=5;i++){
+
+		for(int space=1;space<=i;space++){
+
+			printf(" ");
+		}
+		for(int j=5;j>=i;j--){
+
+			printf("*");
+		}
+		printf("\n");
+	}
+ 	return 0;
+}
