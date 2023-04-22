53ef7feb10df86f52340c2fed03ca62bd68f7591
PrathmShinde <99710720+PrathmShinde@users.noreply.github.com>
2023-04-22 18:13:45 +0530

    Add files via upload

---
+++ PatternPrograms/star8.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main(){
+
+        int i,j,space;
+        int rows=5;
+
+ for(i=1;i<=5;i++){
+                for(j=i;j<=5;j++){
+                        printf("*");
+                        } 
+                            printf("\n");
+                            } 
+                              for(i=5;i>=1;i--){
+                                       for(j=6;j>=i;j--){
+                                              
+					         printf("*");
+                                                 }
+                                                      printf("\n");
+                                                      } 
+                                                          return 0;
+                                                               }
+
