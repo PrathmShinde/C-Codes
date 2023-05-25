ec807a67796b9058bf5487e0daf610c31b37d436
Prathmesh Shinde <99710720+PrathmShinde@users.noreply.github.com>
2023-05-25 09:19:57 -0700

    Add files via upload

---
+++ array/array5.c
@@ -0,0 +1,9 @@
+
+#include<stdio.h>
+
+int main() {
+
+	int array1[5] = { 'a','b','c','d','e'};
+
+	printf("%ld\n",sizeof(array1));           // there is not imp what you are initialize in { }, imp is what type of array you declared.
+}
