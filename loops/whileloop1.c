f7f9eeafd0f95f8a490581d12d6fb1cdc64c60c5
Prathmesh Shinde <99710720+PrathmShinde@users.noreply.github.com>
2023-06-04 05:12:07 -0800

    Add files via upload

---
+++ loops/whileloop1.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(){
+
+	int i=1,a,sum;
+
+	while(i<=20){
+
+		if(i%2==0){
+			sum=sum+i;	
+              	
+		printf("%d\n",sum);
+		}
+               // printf("Total : %d\n",sum);
+                i++;
+		
+	}
+       		         
+
+}
