2844309f7427d92bf53e0de3c070092657492f7e
Prathmesh Shinde <99710720+PrathmShinde@users.noreply.github.com>
2023-06-08 05:46:32 -0800

    Add files via upload

---
+++ array/arraysize.c
@@ -0,0 +1,20 @@
+// Calculate size of array
+
+
+#include<stdio.h>
+
+int main() {
+
+	int age[5];
+	char ch[10];
+	float fno[5];
+	double db[5];
+
+
+
+ printf("%ld\n",sizeof(age));
+ printf("%ld\n",sizeof(ch));
+ printf("%ld\n",sizeof(fno));
+ printf("%ld\n",sizeof(db));
+
+} 
