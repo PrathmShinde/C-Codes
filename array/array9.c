371a6c3d6c2162d3a484d64538903aa5da6afe82
Prathmesh Shinde <99710720+PrathmShinde@users.noreply.github.com>
2023-06-08 05:46:15 -0800

    Add files via upload

---
+++ array/array9.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+void main() {
+
+	int a= 5;
+
+	int num[a] = {5,6,7};
+
+	printf("%d\n",num[0]);
+	printf("%d\n",num[1]);
+	printf("%d\n",num[2]);
+	printf("%d\n",num[3]);
+	printf("%d\n",num[4]);
+}
