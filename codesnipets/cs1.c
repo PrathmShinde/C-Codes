d66754b6da1923ddcdd0545a61477adfddc5810b
Prathmesh Shinde <99710720+PrathmShinde@users.noreply.github.com>
2023-06-19 06:15:16 -0800

    Add files via upload

---
+++ codesnipets/cs1.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(){
+
+	int a=2;
+	switch(a)
+	{
+		case '1' : printf("case1\n");
+			   break;
+                
+		case '2' : printf("case2\n");
+
+                           break;
+
+		case '3' : printf("case3\n");
+                           break;
+
+	        default:  printf("inside default case\n");
+	}
+}
