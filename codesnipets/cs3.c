db3c14db68f5f86574eefae8b808531609d3c781
Prathmesh Shinde <99710720+PrathmShinde@users.noreply.github.com>
2023-06-20 08:11:10 -0800

    Add files via upload

---
+++ codesnipets/cs3.c
@@ -0,0 +1,16 @@
+//What will be the output of following c code.
+
+
+#include<stdio.h>
+
+int main(){
+
+	int i=5;
+
+	if(i!=5)
+		printf("inside if block");
+		printf("Enjoying programming\n");
+		printf("out of it\n");
+
+
+}
