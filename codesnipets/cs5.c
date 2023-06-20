dea6d1706cc7b2c60c8f41cd472b09b638c8ed39
Prathmesh Shinde <99710720+PrathmShinde@users.noreply.github.com>
2023-06-20 08:11:54 -0800

    Add files via upload

---
+++ codesnipets/cs5.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int main(){
+
+
+	unsigned char a;
+
+	printf("Enter Number:\n");
+	scanf("%c",&a);
+
+	printf("%c\n",a);
+	
+	
+/*
+	printf("size of int is     : %ld\n",sizeof(int));
+	printf("size of char is    : %ld\n",sizeof(char));
+	printf("size of float is   : %ld\n",sizeof(float));
+	printf("size of double is  : %ld\n",sizeof(double));
+
+*/
+
+      return 0;
+
+
+ 
+}
+
