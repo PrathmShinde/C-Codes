d6aba9b7b94edf363aa2fa076505b5a116a774bd
Prathmesh Shinde <99710720+PrathmShinde@users.noreply.github.com>
2023-06-23 06:11:44 -0800

    Add files via upload

---
+++ codesnipets/gets.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+
+       char c[25];
+       printf("Enter a string : ");
+       
+       gets(c);
+       
+       printf("\n%s is awesome ",c);
+       
+       return 0;
+}
