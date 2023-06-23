a872c58109022a02d98f067f5b428b6a3740c3ae
Prathmesh Shinde <99710720+PrathmShinde@users.noreply.github.com>
2023-06-23 06:10:26 -0800

    Add files via upload

---
+++ codesnipets/exit.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+         printf("This statement is before exit(); function ");
+
+	 exit(0);
+
+	 printf("It will not display ");
+
+	 return 0;
+}
+
+/*
+ *
+Note:
+
+exit (some numeric value); will exit the program.
+
+*/
