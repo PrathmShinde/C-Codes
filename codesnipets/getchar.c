d59d9d256b9b53e5c98b89ed29dc9b1651f45746
Prathmesh Shinde <99710720+PrathmShinde@users.noreply.github.com>
2023-06-23 06:11:25 -0800

    Add files via upload

---
+++ codesnipets/getchar.c
@@ -0,0 +1,13 @@
+#include<stdio.h> 
+
+int main(){
+
+            char c;
+            printf("Enter a character : ");
+
+            c = getchar();
+            printf("\nEntered character : %c ", c);
+
+            return 0;
+}
+
