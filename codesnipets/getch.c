29140bf7dc4f6aa026d9fe89c19a6ee6ac327940
Prathmesh Shinde <99710720+PrathmShinde@users.noreply.github.com>
2023-06-23 06:11:08 -0800

    Add files via upload

---
+++ codesnipets/getch.c
@@ -0,0 +1,12 @@
+#include <stdio.h> 
+//#include <conio.h> 
+
+int main(){
+
+          printf("\nHello, press any alphanumeric character to exit ");
+	
+	  getch();
+	  getche();
+	  
+	  return 0;
+}
