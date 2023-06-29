612db27f7b6f77060f949ad0dcaba0dc5531ee55
Prathmesh Shinde <99710720+PrathmShinde@users.noreply.github.com>
2023-06-29 11:18:33 +0530

    Add files via upload

---
+++ codesnipets/ifDemo3.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+ int  main(){
+
+	 int mark=93;
+
+
+	 if(mark>90){
+		 printf("marks is grater than 90\n");
+	         printf("Core2web\n");
+	 }
+          printf("You Must Know The Code, Till The Core\n"); 
+ }
