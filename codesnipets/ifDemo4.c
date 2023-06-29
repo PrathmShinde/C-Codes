e9f789f81d1a85dc3a251ab0b6c5bc812910a541
Prathmesh Shinde <99710720+PrathmShinde@users.noreply.github.com>
2023-06-29 11:18:51 +0530

    Add files via upload

---
+++ codesnipets/ifDemo4.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+ int  main(){
+
+	 int mark=93;
+
+
+	 if(mark>90);{                                    
+		 printf("marks is grater than 90\n");
+		 printf("Core2web\n");
+		 
+		                                                 
+            }
+          printf("You Must Know The Code, Till The Core\n"); 
+ }
+	 
+
+// Here, ' ; ' consider as a Null Statement. therefore after if statement next statement is null.
+// statement. And null Statement was execute.and then remaining all statements are executed.
+//
+// The remaining statements are not in the if block.
+// They have a different seperate block. and this block will be executed
