78f9a82764d266938f5aff529209ca69a5d1ebc4
Prathmesh Shinde <99710720+PrathmShinde@users.noreply.github.com>
2023-07-01 20:50:16 +0530

    Add files via upload

---
+++ codesnipets/sleep.c
@@ -0,0 +1,26 @@
+#include <stdio.h> 
+#include <unistd.h>
+
+int main(){
+
+      printf("Countdown... ");
+      
+      printf("\n 3");
+  sleep(1);
+     printf("\n 2");
+  sleep(1);
+     printf("\n 1");
+  sleep(1);
+     printf("\n Celebration Time ");
+
+     return 0;
+}
+
+
+/*
+ *
+Note:
+
+sleep (seconds); will delay the program's output with respect to seconds which you mentioned.
+
+*/
