cdafa27a895ffc2904db53f3d3ead91d2bbb3f4d
Prathmesh Shinde <99710720+PrathmShinde@users.noreply.github.com>
2023-07-01 20:50:54 +0530

    Add files via upload

---
+++ codesnipets/typecast.c
@@ -0,0 +1,35 @@
+
+#include<stdio.h>
+
+int main(){
+
+/*
+         int i=65;
+         char c;
+         c=(char)i;
+
+          printf("%c\n",c);
+*/
+
+  int a=10;
+  int b=5;
+//int c;
+
+  printf("division=%f\n",(float)a/b);
+
+
+  return 0;
+
+}
+
+/*
+ * #include <stdio.h>
+int main()
+{
+float a = 3.56;
+printf("The value of a = %d ",(int)a);
+return 0;
+}
+
+*/
+
