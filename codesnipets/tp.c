3a2af0288623915c842c49d4cf7dc39b22bdacc9
Prathmesh Shinde <99710720+PrathmShinde@users.noreply.github.com>
2023-07-01 20:50:36 +0530

    Add files via upload

---
+++ codesnipets/tp.c
@@ -0,0 +1,12 @@
+#include<stdio.h> 
+
+int main(){
+
+            char c;
+            printf("Enter a character : ");
+
+	    c = getchar();
+	    printf("\nEntered character : %c ", c);
+	    
+	    return 0;
+}
