942e89c999a774b42472957ff3e6cfbb1ab735eb
Prathmesh Shinde <99710720+PrathmShinde@users.noreply.github.com>
2023-07-04 22:34:53 +0530

    Add files via upload

---
+++ trial&error/forloop1.c
@@ -0,0 +1,17 @@
+
+
+#include<stdio.h>
+
+int main(){
+
+	int i=0;
+	
+	for(;i;){       // if we remove this i used to check condition, then loop will be runs infinitly
+		printf("Here is some mail for you\n");
+	}
+	return 0;
+}
+
+
+
+// No Output 
