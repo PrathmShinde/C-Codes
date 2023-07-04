c77acf7da2cb187b141abbdac43c1c910efa0f74
Prathmesh Shinde <99710720+PrathmShinde@users.noreply.github.com>
2023-07-04 22:34:22 +0530

    Add files via upload

---
+++ trial&error/forloop.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+
+        int i=65;
+
+        for(;i<=90;i++);{
+
+            printf("out of for loop\n");
+           
+            
+        }
+    return 0;
+}
+
