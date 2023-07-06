e7a2178e4d33f9849656e7d3ebed65845ac4fdc0
Prathmesh Shinde <99710720+PrathmShinde@users.noreply.github.com>
2023-07-06 22:52:32 +0530

    Add files via upload

---
+++ trial&error/forloop4.c
@@ -0,0 +1,15 @@
+//  How many times the while loop in the following C code will get executed ?
+
+
+#include<stdio.h>
+
+int main(){
+
+	int j=1;
+
+	while(j<=255);{
+
+		printf("%d %c",j,j);
+		j++;
+	}
+}
