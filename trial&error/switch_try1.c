e323e6ee3b08aabcb769a8226811a62f82b0df90
Prathmesh Shinde <99710720+PrathmShinde@users.noreply.github.com>
2023-07-07 21:50:08 +0530

    Add files via upload

---
+++ trial&error/switch_try1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(){
+
+	int i=1;
+
+	switch(i-2){
+
+		case 1 : 
+			printf("Konnichiwa\n");
+			break;
+	
+		case -1 : 
+			printf("Arigato Gozaimasu\n");
+			break;
+	}
+	return 0;
+}
