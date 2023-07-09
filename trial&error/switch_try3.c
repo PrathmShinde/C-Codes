e43d0d27298eef36237e293ee69ea3cfee3b917a
Prathmesh Shinde <99710720+PrathmShinde@users.noreply.github.com>
2023-07-09 21:38:22 +0530

    Add files via upload

---
+++ trial&error/switch_try3.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main(){
+
+	int temp;
+
+	scanf("%d",&temp);
+
+	switch(temp){
+
+		case (temp<=20) :
+			printf("Ooooooohhhh!Damn cool!\n");
+			break;
+/*		case (temp>20 && temp<=30) :
+			printf("Rain rain here Again\n");
+			break;
+		case (temp>30 && temp<=40) :
+			printf("Rain rain here Again\n");
+			break;
+		default:
+			printf("Arigazo Gozaimasu\n");
+
+*/
+	}
+	return 0;
+}
