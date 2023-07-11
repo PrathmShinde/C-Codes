8deb2616e341b169641b7e89f801b0425d743e51
Prathmesh Shinde <99710720+PrathmShinde@users.noreply.github.com>
2023-07-11 22:58:48 +0530

    Add files via upload

---
+++ trial&error/try3.c
@@ -0,0 +1,34 @@
+/*  Formula:   ~(positive) = -(positive)
+ *
+ *             ~(35)       = -(36)
+
+        
+                 35 = ~(35) = -36
+
+
+*/		 
+
+
+
+#include<stdio.h>
+
+int main(){
+
+
+	int a=35;
+	int b=-35;
+	int c=20;
+	int d=-20;
+
+
+
+	printf("%d\n",~(a));
+	printf("%d\n",~(b));
+	printf("%d\n",~(c));
+	printf("%d\n",~(d));
+
+
+}
+
+
+
