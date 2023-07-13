65668d4b425466b9385b53c71fef38edc58f718f
Prathmesh Shinde <99710720+PrathmShinde@users.noreply.github.com>
2023-07-13 22:02:46 +0530

    Add files via upload

---
+++ trial&error/try4.c
@@ -0,0 +1,37 @@
+
+
+#include<stdio.h>
+
+int main(){
+
+
+    int mark=65, compmark=90;
+
+    if(mark>70){
+	    
+	    printf("modern clg\n");
+    
+             if(compmark>80)
+	              printf("FY-BCS\n");
+              else
+	              printf("SY-BCS\n");
+             
+	     
+    }
+    else
+	    printf("sportman\n");
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+}
