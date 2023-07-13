0d12976ac7102428ef23d690df8b3ebff94cf8ae
Prathmesh Shinde <99710720+PrathmShinde@users.noreply.github.com>
2023-07-13 22:03:12 +0530

    Add files via upload

---
+++ trial&error/try5.c
@@ -0,0 +1,25 @@
+
+
+#include<stdio.h>
+
+int main(){
+
+	int c=3;
+         float f= 20.5;
+         int total =5;
+	switch(2){
+
+	case 1  :  printf("GameChanger\n");
+              	
+	case 2  :  printf("PRATHMESH\n");	 
+	case 3  : printf("\n"); 
+	 
+               
+	default: --total;
+		 printf("%d\n",total);
+	}
+         //  printf("JKMP\n");
+        
+           
+
+}
