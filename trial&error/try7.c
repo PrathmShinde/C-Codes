5848efcc8b3d30b6443c04bcf91a2651bb2c82ec
Prathmesh Shinde <99710720+PrathmShinde@users.noreply.github.com>
2023-07-15 22:17:44 +0530

    Add files via upload

---
+++ trial&error/try7.c
@@ -0,0 +1,23 @@
+
+#include<stdio.h>
+
+int main(){
+
+
+
+	int a=65;
+
+	switch(a){
+
+        case 'A': printf("AAAAA\n");
+		   break;
+        case 'B' : printf("BBBBB\n");
+		   break;
+        case '65' : printf("aaaaa\n");
+		   break;
+
+	default:   printf("default\n");
+
+
+	}
+}
