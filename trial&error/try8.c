5a75e05ac00af07a0b31bf20da658a09f9a532b0
Prathmesh Shinde <99710720+PrathmShinde@users.noreply.github.com>
2023-07-15 22:18:03 +0530

    Add files via upload

---
+++ trial&error/try8.c
@@ -0,0 +1,21 @@
+
+
+#include<stdio.h>
+
+int main(){
+
+
+    int	x=10;
+
+
+    switch(x){
+
+	    case 5+5:   printf("%d",5+5);
+			break;
+
+	    case 6+4:   printf("%d",5+5);
+			break;
+
+	    default:    printf("default\n");
+    }
+}
