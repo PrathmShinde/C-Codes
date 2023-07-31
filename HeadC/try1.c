5ff9e867e652a42881a1fa7d3d6b6be8bc7fce81
Prathmesh Shinde <99710720+PrathmShinde@users.noreply.github.com>
2023-07-31 14:04:01 +0530

    Add files via upload

---
+++ HeadC/try1.c
@@ -0,0 +1,16 @@
+// Accept String using scanf()
+
+
+#include<stdio.h>
+
+int main() {
+
+	char exe[20] ;
+
+	puts("Enters boyfrirnds name :");
+	scanf("%19s",exe);	
+
+	printf("Dear %s. \n\n\t You're history. \n",exe);
+
+	return 1;
+}
