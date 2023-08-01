fa8f0ef8d32f1f6c4a6006c9c1ac2ec4ba6a81f5
Prathmesh Shinde <99710720+PrathmShinde@users.noreply.github.com>
2023-08-01 21:46:05 +0530

    Add files via upload

---
+++ HeadC/try4.c
@@ -0,0 +1,21 @@
+// function
+
+
+#include<stdio.h>
+
+	int larger(int a,int b) {
+
+		if(a > b)
+			return a;
+		return b;
+	}
+
+	int main() {
+		
+		int greatest = larger(100,1000) ;
+
+		printf("%d is the greatest\n",greatest);
+
+		return 0;
+	}
+
