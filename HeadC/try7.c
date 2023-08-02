ab1ba9998f594601ab159844e977035362fced9f
Prathmesh Shinde <99710720+PrathmShinde@users.noreply.github.com>
2023-08-02 22:17:29 +0530

    Add files via upload

---
+++ HeadC/try7.c
@@ -0,0 +1,15 @@
+//how to see variableaddress 
+
+#include<stdio.h>
+
+int main() {
+
+	int x=4;
+	int y=5;
+
+	printf("x is stored at %p\n",&x);
+	printf("y is stored at %p\n",&y);
+
+	return 0;
+
+}
